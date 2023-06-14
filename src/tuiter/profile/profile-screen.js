ade166e29b4467444ac5cc0a2742aa996bd66466
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-13 20:43:59 -0400

    Assignment 5 Final Submission 50

--- src/tuiter/profile/profile-screen.js
+++ src/tuiter/profile/profile-screen.js
@@ -29,7 +29,7 @@ console.log("profile" , profile)
      <h1>Profile Screen</h1>
      {profile && (<div>
        <div>
-        <label>First Name</label>
+        <label><span>First Name</span></label>
         <input type="text" value={profile.firstName}
          onChange={(event) => {
           const newProfile = {
