f36a51a3afc96bf1da31433e45734abe41e2488e
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-20 16:05:34 -0400

    a6 8

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 const USERS_URL = `${SERVER_API_URL}/users`;
-// const USERS_URL = `http://localhost:4000/api/users`;
 
 
 const api = axios.create({ withCredentials: true });
@@ -26,8 +25,9 @@ export const logout = async () => {
     return response.data;
    };
    export const updateUser = async (user) => {
+    console.log(user._id);
     console.log("id", `${USERS_URL}/update`, user);
-    const response = await api.put(`${USERS_URL}`, {user});
+    const response = await api.put(`${USERS_URL}/update/${user._id}`, {user});
     return response.data;
    };
    export const register = async ({ username, password }) => { 
